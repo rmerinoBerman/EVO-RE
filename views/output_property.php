a62b9583b2b5499155bb26c1ff8d2464a3a46e50
Rene Merino <renemerino@live.com>
2014-01-03 11:18:29 -0500

    Added listing views templates

---
+++ views/output_property.php
@@ -0,0 +1,12 @@
+<div class="property">
+	<div class="property-photo">
+		<img src="" alt="">
+	</div>
+	<div class="property-specs">
+		<div class="property-title">
+			<h3></h3>
+		</div>
+		<div class="dimenssions"></div>
+		<a href="" class="details">View Details</a>
+	</div>
+</div>
---
+++ views/page_listing.php
@@ -0,0 +1,5 @@
+<div class="properties-listing">
+	<h2 class="page-title"></h2>
+	<div class="property-search"></div>
+	<div class="listing"></div>
+</div>
