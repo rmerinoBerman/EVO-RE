ec93e9de7de61a46927b39bce5e5b167855b73e4
rmerinoBerman <rmerino@bermangrp.com>
2014-01-06 12:33:55 -0500

    Updated Inner Page View

--- views/page_inner.php
+++ views/page_inner.php
@@ -1,12 +1,14 @@
-<div class="pageInfo"></div>
-<div class="mainContent">
-	<div class="searchResults"></div>
-</div>
-<div class="sideBarWrapper">
-	<div class="sideBar">
-		<div class="sideNav"></div>
-		<div class="search">
-			<input type="text" placeholder="Search">
+<div class="container container-twelve">
+	<div class="three columns">
+		<div class="sideBar">
+			<div class="sideNav"></div>
+		</div>
+	</div>
+	<div class="nine columns">
+		<div class="mainContent">
+			<div class="pageInfo"></div>
+			<div class="content-entry"></div>
+			<div class="searchResults"></div>
 		</div>
 	</div>
 </div>
